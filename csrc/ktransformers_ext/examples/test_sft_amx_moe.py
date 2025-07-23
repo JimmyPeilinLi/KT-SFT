fix allocate up/gate/down_t_bb, due to the (n, k); fix the using adapter from local_chat; add debug codes; remain wrong value of amx matmul and convert adapter.bin to gguf

csrc/ktransformers_ext/examples/test_sft_amx_moe.py
@@ -363,8 +363,6 @@ def test_amx_moe_two_round():
         out_ref = moe_torch(input_pt, expert_ids, weights,
                             gate_proj, up_proj, down_proj, True)
         out_ref.retain_grad()
-        
-        batch_size_tensor = torch.tensor([qlen], dtype=torch.int32).contiguous()
 
         # 缓存forward中间结果用于python backward
         gate_u_cache = []
@@ -391,7 +389,7 @@ def test_amx_moe_two_round():
         cpu_infer.submit(moe_cpp.forward(
             qlen, n_routed_experts,
             expert_ids.data_ptr(), weights.data_ptr(),
-            input_cpp.data_ptr(), out_cpp.data_ptr(), batch_size_tensor.data_ptr()))
+            input_cpp.data_ptr(), out_cpp.data_ptr()))
         cpu_infer.sync()
         print("After forward")
         t1 = time.time()
@@ -407,14 +405,16 @@ def test_amx_moe_two_round():
         grad_in_cpp  = torch.zeros_like(input_cpp, dtype=gradtype).contiguous()
 
         # # Torch backward
-        # for p in (gate_proj, up_proj, down_proj, input_pt):
-        #     if p.grad is not None:
-        #         p.grad.zero_()
-        # t2 = time.time()
-        # out_ref.backward(grad_out, retain_graph=True)
-        # t3 = time.time()
-        # print(f"PyTorch backward time {t3-t2:.4f}s | "
-        #       f"TFLOPS {flop_bwd/(t3-t2)/1e12:.2f}")
+        for p in (gate_proj, up_proj, down_proj, input_pt):
+            if p.grad is not None:
+                p.grad.zero_()
+        t2 = time.time()
+        print(1111111)
+        out_ref.backward(grad_out, retain_graph=True)
+        print(2222222)
+        t3 = time.time()
+        print(f"PyTorch backward time {t3-t2:.4f}s | "
+              f"TFLOPS {flop_bwd/(t3-t2)/1e12:.2f}")
 
         # Python backward（模拟C++逻辑）- 详细版本
         t4_py = time.time()
@@ -433,37 +433,37 @@ def test_amx_moe_two_round():
             qlen, n_routed_experts,
             expert_ids.data_ptr(), weights.data_ptr(), input_cpp.data_ptr(),
             grad_out_cpp.data_ptr(),
-            grad_in_cpp.data_ptr(), batch_size_tensor.data_ptr()))
+            grad_in_cpp.data_ptr()))
         cpu_infer.sync()
         t5 = time.time()
         print("After backward")
         print(f"C++      backward time {t5-t4:.4f}s | "
               f"TFLOPS {flop_bwd/(t5-t4)/1e12:.2f}")
 
-        # # 三种backward结果对比
-        # gcpp = grad_in_cpp.to(torch.float32)
-        # gref = input_pt.grad.to(torch.float32) if input_pt.grad is not None else torch.zeros_like(input_pt, dtype=torch.float32)
-        # gpy = grad_in_python.to(torch.float32)
+        # 三种backward结果对比
+        gcpp = grad_in_cpp.to(torch.float32)
+        gref = input_pt.grad.to(torch.float32) if input_pt.grad is not None else torch.zeros_like(input_pt, dtype=torch.float32)
+        gpy = grad_in_python.to(torch.float32)
         
-        # print(f"C++ AMX backward:{gcpp}", '\n', f"pytorch backward:{gref}", '\n', f"python backward:{gpy}")
+        print(f"C++ AMX backward:{gcpp}", '\n', '\n', f"python backward:{gpy}")
         
-        # # 对比结果
-        # rel_bwd_cpp = (gcpp - gref).abs().mean() / gref.abs().mean()
-        # rel_bwd_py = (gpy - gref).abs().mean() / gref.abs().mean()
-        # rel_bwd_cpp_py = (gcpp - gpy).abs().mean() / gpy.abs().mean()
+        # 对比结果
+        rel_bwd_cpp = (gcpp - gref).abs().mean() / gref.abs().mean()
+        rel_bwd_py = (gpy - gref).abs().mean() / gref.abs().mean()
+        rel_bwd_cpp_py = (gcpp - gpy).abs().mean() / gpy.abs().mean()
         
-        # print(f"Torch vs C++:    {rel_bwd_cpp.item():.3e}")
-        # print(f"Torch vs Python: {rel_bwd_py.item():.3e}")
-        # print(f"C++ vs Python:   {rel_bwd_cpp_py.item():.3e}")
+        print(f"Torch vs C++:    {rel_bwd_cpp.item():.3e}")
+        print(f"Torch vs Python: {rel_bwd_py.item():.3e}")
+        print(f"C++ vs Python:   {rel_bwd_cpp_py.item():.3e}")
         
-        # # 检查是否对拍成功
-        # if rel_bwd_cpp_py.item() < 1e-5:
-        #     print("✅ C++和Python backward对拍成功!")
-        # else:
-        #     print("❌ C++和Python backward对拍失败，存在显著差异")
+        # 检查是否对拍成功
+        if rel_bwd_cpp_py.item() < 1e-5:
+            print("✅ C++和Python backward对拍成功!")
+        else:
+            print("❌ C++和Python backward对拍失败，存在显著差异")
             
         
-        manual_check(expert_ids)
+        # manual_check(expert_ids)
 
 def load_bf16(stub, shape):
     with open(stub + ".bf16", "rb") as f:

ktransformers/local_chat.py
@@ -101,6 +101,11 @@ def local_chat(
 
     if not is_sft:
         torch.set_grad_enabled(False)
+        
+    if is_sft == True or use_adapter == True:
+        GLOBAL_CONFIG._config["mod"] = "sft"
+    else:
+        GLOBAL_CONFIG._config["mod"] = "infer"
 
     Config().cpu_infer = cpu_infer
     Config().chunk_size = chunk_size
@@ -156,6 +161,8 @@ def local_chat(
         gguf_path = input(
             "please input the path of your gguf file(gguf file in the dir containing input gguf file must all belong to current model):"
         )
+        
+    GLOBAL_CONFIG._config["mod"] = "infer"
     optimize_and_load_gguf(model, optimize_config_path, gguf_path, config)
 
     # print_module_tree(model)  # 观察输出是否有重复模块或循环
@@ -169,6 +176,7 @@ def local_chat(
         lora_and_load_adapter(model, tokenizer, sft_data_path, save_adapter_path)
 
     if use_adapter == True:
+        GLOBAL_CONFIG._config["mod"] = "sft"
         if is_sft == True:
             raise AttributeError("We do not support more than one adapter up to now...")
         
@@ -296,26 +304,20 @@ if __name__ == "__main__":
             use_adapter=args.use_adapter,
             use_adapter_path=args.use_adapter_path
         )
-        
-        if args.is_sft == True:
-            GLOBAL_CONFIG._config["mod"] = "sft"
-        else:
-            GLOBAL_CONFIG._config["mod"] = "infer"
 
     else:
         local_chat(
             model_path="/mnt/data/models/DeepSeek-V2-Lite-Chat",
             model_config_path="ktransformers/configs/model_config",
-            gguf_path="/mnt/data/models/DeepSeek-V2-Lite-Chat-GGUF-FP16/",
+            gguf_path="/mnt/data/models/DeepSeek-V2-Lite-Chat/",
             cpu_infer=112,
             max_new_tokens=1000,
             force_think=False,
-            optimize_config_path="ktransformers/optimize/optimize_rules/DeepSeek-V2-Lite-Chat-sft.yaml",
-            is_sft=True,
-            sft_data_path="tmp/demo_test_10example.json",
+            optimize_config_path="ktransformers/optimize/optimize_rules/DeepSeek-V2-Lite-Chat-sft-amx.yaml",
+            is_sft=False,
+            sft_data_path="test_adapter/sft_translation.json",
             save_adapter_path="test_adapter/demo_adapter_KT_target_kv",
-            use_adapter=False,
-            use_adapter_path="test_adapter/demo_adapter_origin_target_kv"
+            use_adapter=True,
+            use_adapter_path="/home/lpl/KT-beifen/test_adapter/demo_adapter_origin_target_kv/lora.gguf"
         )
         
-        GLOBAL_CONFIG._config["mod"] = "sft"

ktransformers/operators/experts.py
@@ -593,7 +593,7 @@ class KSFTExpertsCPU(torch.autograd.Function):
                     expert_ids.data_ptr(), 
                     weights.data_ptr(), 
                     input_tensor.data_ptr(), 
-                    output.data_ptr()
+                    output.data_ptr(),
                 )
             )
             cpu_infer.sync()
@@ -608,19 +608,19 @@ class KSFTExpertsCPU(torch.autograd.Function):
         ctx.layer_idx = layer_idx
         
         # ---------- FLOPs ----------
-        qlen = expert_ids.size(0)
-        k    = expert_ids.size(1)
+        # qlen = expert_ids.size(0)
+        # k    = expert_ids.size(1)
 
-        flops_fwd = 6 * qlen * k * H_FIXED * M_FIXED # 2（2 次乘加）* 3（三个矩阵）= 6
-        tflops_f  = flops_fwd / t_fwd / 1e12
+        # flops_fwd = 6 * qlen * k * H_FIXED * M_FIXED # 2（2 次乘加）* 3（三个矩阵）= 6
+        # tflops_f  = flops_fwd / t_fwd / 1e12
 
-        # 把 qlen / k 留给 backward
-        ctx.saved_dims = (qlen, k)
-        ctx._time_fwd  = t_fwd
-        print(f"qlen ,k:{qlen}, {k}")
+        # # 把 qlen / k 留给 backward
+        # ctx.saved_dims = (qlen, k)
+        # ctx._time_fwd  = t_fwd
+        # print(f"qlen ,k:{qlen}, {k}")
         
-        print(f"[KSFTExpertsCPU] Forward  : {flops_fwd/1e9:.3f} GFLOPs | "
-              f"{tflops_f:.2f} TFLOPS ({t_fwd*1e3:.2f} ms)")
+        # print(f"[KSFTExpertsCPU] Forward  : {flops_fwd/1e9:.3f} GFLOPs | "
+            #   f"{tflops_f:.2f} TFLOPS ({t_fwd*1e3:.2f} ms)")
 
         return result
         
@@ -648,7 +648,7 @@ class KSFTExpertsCPU(torch.autograd.Function):
         bw_start = time.time()
         ctx.cpu_infer.submit(
             ctx.moe.backward(
-                layer_idx,
+                # layer_idx,
                 output_grad.size(0),  # qlen
                 expert_ids.size(1),   # k
                 expert_ids.data_ptr(),
@@ -656,22 +656,21 @@ class KSFTExpertsCPU(torch.autograd.Function):
                 input_tensor.data_ptr(), 
                 output_grad.data_ptr(),
                 input_grad.data_ptr(),
-                []
             )
         )
         ctx.cpu_infer.sync()
         
         bw_end   = time.time()
-        t_bw    = bw_end - bw_start
+        # t_bw    = bw_end - bw_start
         
-        # ---------- FLOPs ----------
-        qlen, k  = ctx.saved_dims          # 正确的 q / k
-        flops_bw = 18 * qlen * k * H_FIXED * M_FIXED
-        tflops_b = flops_bw / t_bw / 1e12
-        print(f"qlen:{qlen}, k:{k}")
+        # # ---------- FLOPs ----------
+        # qlen, k  = ctx.saved_dims          # 正确的 q / k
+        # flops_bw = 10 * qlen * k * H_FIXED * M_FIXED
+        # tflops_b = flops_bw / t_bw / 1e12
+        # print(f"qlen:{qlen}, k:{k}")
 
-        print(f"[KSFTExpertsCPU] Backward : {flops_bw/1e9:.3f} GFLOPs | "
-              f"{tflops_b:.2f} TFLOPS ({t_bw*1e3:.2f} ms)")
+        # print(f"[KSFTExpertsCPU] Backward : {flops_bw/1e9:.3f} GFLOPs | "
+        #       f"{tflops_b:.2f} TFLOPS ({t_bw*1e3:.2f} ms)")
         
         return input_grad.to(device=ctx.out_device), None, None, None, None, None, None
     

ktransformers/operators/linear.py
@@ -174,39 +174,21 @@ class KLinearTorch(KLinearBase):
         if w is None: w = self.load_weight(device=device)
         # else: self.out_features = w.shape[0], self.in_features = w.shape[1]
         
-        if GLOBAL_CONFIG._config["mod"] == "infer":
-            if isinstance(w, nn.Parameter):
-                try:
-                    self.weight = w.to(dtype=self.dtype).view(self.out_features, self.in_features).T
-                except: 
-                    self.weight = w.to(dtype=self.dtype).T
-                self.has_bias = False
-            elif isinstance(w, tuple):
-                try:
-                    self.weight = w[0].to(dtype=self.dtype).view(self.out_features, self.in_features).T
-                except:
-                    self.weight = w[0].to(dtype=self.dtype).T
-                self.bias = w[1].to(dtype=self.dtype)
-                self.has_bias = True
-            else:
-                raise ValueError("Invalid weight type")
-
-        elif GLOBAL_CONFIG._config["mod"] == "sft":
-            if isinstance(w, nn.Parameter):
-                try:
-                    self.weight = nn.Parameter(w.to(dtype=self.dtype).view(self.out_features, self.in_features).T, requires_grad=True)
-                except: 
-                    self.weight = nn.Parameter(w.to(dtype=self.dtype).T, requires_grad=True)
-                self.has_bias = False
-            elif isinstance(w, tuple):
-                try:
-                    self.weight = nn.Parameter(w[0].to(dtype=self.dtype).view(self.out_features, self.in_features).T, requires_grad=True)
-                except:
-                    self.weight = nn.Parameter(w[0].to(dtype=self.dtype).T, requires_grad=True)
-                self.bias = nn.Parameter(w[1].to(dtype=self.dtype), requires_grad=True)
-                self.has_bias = True
-            else:
-                raise ValueError("Invalid weight type")
+        if isinstance(w, nn.Parameter):
+            try:
+                self.weight = w.to(dtype=self.dtype).view(self.out_features, self.in_features).T
+            except: 
+                self.weight = w.to(dtype=self.dtype).T
+            self.has_bias = False
+        elif isinstance(w, tuple):
+            try:
+                self.weight = w[0].to(dtype=self.dtype).view(self.out_features, self.in_features).T
+            except:
+                self.weight = w[0].to(dtype=self.dtype).T
+            self.bias = w[1].to(dtype=self.dtype)
+            self.has_bias = True
+        else:
+            raise ValueError("Invalid weight type")
         # self.linear = self.linear.to(device)
         self.weight = self.weight.to(device)
         if self.has_bias:

ktransformers/sft/lora.py
@@ -65,13 +65,10 @@ def preprocess_function(examples, tokenizer):
 
 class ModifiedTrainer(Trainer):
     def save_model(self, output_dir=None, _internal_call=False):
-        # 改写trainer的save_model，在checkpoint的时候只存lora权重
+        output_dir = output_dir or self.args.output_dir
         os.makedirs(output_dir, exist_ok=True)
-        torch.save(self.args, os.path.join(output_dir, TRAINING_ARGS_NAME))
-        saved_params = {
-            k: v.to("cpu") for k, v in self.model.named_parameters() if v.requires_grad
-        }
-        torch.save(saved_params, os.path.join(output_dir, "adapter_model.bin"))
+        # 只保存 LoRA adapter（含 adapter_config.json）
+        self.model.save_pretrained(output_dir)
 
 def inspect_device(model, write_file):
     for name, module in model.named_modules(): 